268acb03a07cb627eb2ce73aa1fa502ef2a1686e
Manuel Garcia <manu@Manuels-MacBook-Air.local>
2022-04-10 01:08:39 -0300

    TP_1

--- TP_1/src/TP1.c
+++ TP_1/src/TP1.c
@@ -31,6 +31,9 @@ int main(void) {
 	float precioUnitarioAA;
 	float precioUnitarioLTM;
 	float diferenciaPrecio;
+	int flagKm = 1;
+	int flagPrecio = 1;
+	int flagCostos = 1;
 
 	do {
 		opcion = menu();
@@ -39,17 +42,26 @@ int main(void) {
 		case 1:
 			printf("\nUsted eligio: Ingresar Kilometros\n");
 			kilometros = IngresarFlotante("\nIngrese los kilometros: ");
+			flagKm = 0;
 			break;
 		case 2:
 			printf("\nUsted eligio: Ingresar precio de vuelos\n");
-
+			if(flagKm !=0){
+				printf("\nError. No se puede ingresar el precio del vuelo sin ingresar antes los kilometros\n");
+			}else{
 			aerolineas = IngresarFlotante("\nPrecio Aerolineas: \n");
 			latam = IngresarFlotante("\nPrecio de LATAM: \n");
+			flagPrecio = 0;
+			}
 			break;
 		case 3:
+			if(flagKm != 0 && flagPrecio != 0){
+				printf("Error. No se pueden calcular los costos sin haber ingresado los kilometros"
+						" y los precios de los vuelos");
+			}else if (flagPrecio != 0){
+				printf("\nError. No se puede calcular los costos sin haber ingresado\nlos precios de los vuelos\n");
+			}else{
 			printf("\nUsted eligio: Calcular costos\n");
-//			CalcularCostos(&kilometros, &aerolineas, &latam, &debitoAA , &debitoLTM, &creditoAA, &creditoLTM, &bitcoinAA,
-//					 &bitcoinLTM, &precioUnitarioAA, &precioUnitarioLTM, &diferenciaPrecio);
 			debitoAA = CalcularTarjetaDebito(aerolineas);
 			debitoLTM = CalcularTarjetaDebito(latam);
 			creditoAA = CalcularTarjetaCredito(aerolineas);
@@ -59,11 +71,21 @@ int main(void) {
 			precioUnitarioAA = CalcularPrecioUnitario(aerolineas,kilometros);
 			precioUnitarioLTM = CalcularPrecioUnitario(latam,kilometros);
 			diferenciaPrecio = CalcularDiferenciaPrecio(latam,aerolineas);
+
+			flagCostos = 0;
+			if(flagCostos == 0){
+				printf("Se han hecho todos los calculos. Seleccione la opcion 4 para ver los resultados");
+			}
+			}
 			break;
 		case 4:
+			if(flagCostos != 0){
+				printf("\nError. No se puede mostrar los rsultados sin haber calculado los costos\n");
+			}else{
 			printf("\nUsted eligio: Informar resultados\n");
 			MostrarResultados(debitoAA,debitoLTM,creditoAA,creditoLTM,bitcoinAA,bitcoinLTM,
 					precioUnitarioAA,precioUnitarioLTM,diferenciaPrecio);
+			}
 			break;
 		case 5:
 			printf("\nUsted eligio: Carga Forzada \n");
